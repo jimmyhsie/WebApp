bf033831106a77c819ab7f41c2d1d9f91b82d545
pickone <jimmyhsie@gmail.com>
2019-12-06 19:03:13 +0800

    build upload

---
+++ build/precache-manifest.90f065302244b9c5b0d5ce1782fd5a07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55969fe21a4bd8709c6d85a0cbd1e78e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c450c534bc9ba1a3e590",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "0bfba9040b82b086c3dd",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "c450c534bc9ba1a3e590",
+    "url": "/static/js/2.c3ea8895.chunk.js"
+  },
+  {
+    "revision": "0bfba9040b82b086c3dd",
+    "url": "/static/js/main.aac9c956.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
